231de8ccc6715e8e7792ac3247a44138d94f8efc
Nick Tomeo <tomeonj@gmail.com>
2017-10-31 13:49:18 -0400

    Fixed y-axis label location issue
    The y-axis labels (superscripts in particular) were being cut off the plot as they were too close to the edge. Added axis labeling outside of the plot() call with mtext() in order to specify line placement, and alter the par(mar=c()) parameters.

--- diagnostic_AQ_plot_function.R
+++ diagnostic_AQ_plot_function.R
@@ -11,9 +11,11 @@
 
 
 diagnostic_AQ_plot <- function(curve_data, fit_data, Photo, PARi, group_id){
-      par(mar=c(4,4,1,1), oma=c(1,1,1,1))
+      par(mar=c(3,3,1,1), oma=c(1,1,1,1))
       for(i in seq_along(1:length(unique(curve_data[[group_id]])))){
-            single_curve <- curve_data[curve_data[[group_id]] == unique(curve_data[[group_id]])[i],]
+            single_curve <- 
+            curve_data[curve_data[[group_id]] == 
+                       unique(curve_data[[group_id]])[i],]
             plot(
                   single_curve[[Photo]] ~ single_curve[[PARi]] ,
                   xlim = c(-2, max(curve_data[[PARi]])), 
@@ -21,12 +23,16 @@ diagnostic_AQ_plot <- function(curve_data, fit_data, Photo, PARi, group_id){
                            max(curve_data[[Photo]]) + 2),
                   pch = 3,
                   cex = 2,
-                  xlab = expression("PARi (µmol photons "*m^-2*" "*s^-1*")"),
-                  ylab = expression("Photo (µmol photons "*m^-2*" "*s^-1*")"),
+                  xlab = "",
+                  ylab = "",
                   main = paste("Data from curve ",
                                as.character(
                                      unique(single_curve[[group_id]])))
             )
+            mtext(expression("Photo (µmol photons "*m^-2*" "*s^-1*")"),
+                  line = 2.4, side = 2)
+            mtext(expression("PARi (µmol photons "*m^-2*" "*s^-1*")"),
+                  line = 2.4, side = 1)
             par(new = TRUE)
             curve(((
                   fit_data$Phi[i] * PARi + fit_data$Asat[i] - 
