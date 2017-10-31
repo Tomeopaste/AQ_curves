c90a76c35d7be01760ec9a055524d796e85ec451
Nick Tomeo <tomeonj@gmail.com>
2017-10-31 15:33:46 -0400

    Save to pdf option added
    There is now the option to save the plots to a pdf rather than having them pop up in the plot window. Presumably, this is useful if a large number of curves are analyzed at once. This requires further testing for robustness. Updating again momentarily with comments and instructions.

--- diagnostic_AQ_plot_function.R
+++ diagnostic_AQ_plot_function.R
@@ -10,12 +10,56 @@
 #     headers for Photo, PARi, and group_id from the curve dataframe
 
 
-diagnostic_AQ_plot <- function(curve_data, fit_data, Photo, PARi, group_id){
-      par(mar=c(3,3,1,1), oma=c(1,1,1,1))
+diagnostic_AQ_plot <- function(curve_data, fit_data, Photo, PARi, group_id,
+                               save_to_pdf = FALSE, save_path, file_name){
+      if(save_to_pdf == FALSE){ 
+            par(mar = c(3, 3, 1, 1), oma = c(1, 1, 1, 1))
+            for(i in seq_along(1:length(unique(curve_data[[group_id]])))){
+                  single_curve <- 
+                        curve_data[curve_data[[group_id]] == 
+                                         unique(curve_data[[group_id]])[i],]
+                  plot(
+                        single_curve[[Photo]] ~ single_curve[[PARi]] ,
+                        xlim = c(-2, max(curve_data[[PARi]])), 
+                        ylim = c(min(curve_data[[Photo]]) - 2,
+                                 max(curve_data[[Photo]]) + 2),
+                        pch = 3,
+                        cex = 2,
+                        xlab = "",
+                        ylab = "",
+                        main = paste("Data from curve ",
+                                     as.character(
+                                           unique(single_curve[[group_id]])))
+                  )
+                  mtext(expression("Photo (µmol photons "*m^-2*" "*s^-1*")"),
+                        line = 2.4, side = 2)
+                  mtext(expression("PARi (µmol photons "*m^-2*" "*s^-1*")"),
+                        line = 2.4, side = 1)
+                  par(new = TRUE)
+                  curve(((
+                        fit_data$Phi[i] * PARi + fit_data$Asat[i] - 
+                              sqrt((fit_data$Phi[i] * PARi + fit_data$Asat[i])^2 - 4 *
+                                         fit_data$Phi[i] * fit_data$theta[i] * PARi *
+                                         fit_data$Asat[i])
+                  ) / (2*fit_data$theta[i]) - fit_data$Rd[i]),
+                  from = 0, to = 1600, 
+                  xname = "PARi",
+                  xlab = "", ylab = "", 
+                  xlim = c(-2, max(curve_data[[PARi]])), 
+                  ylim = c(min(curve_data[[Photo]]) - 2,
+                           max(curve_data[[Photo]]) + 2),
+                  axes = FALSE,
+                  col = "red",
+                  lwd = 2
+                  )
+            }} else{
+             if(dir.exists(save_path)){
+      pdf(paste0(save_path, file_name, ".pdf"))
+      par(mar = c(3, 3, 1, 1), oma = c(1, 1, 1, 1))
       for(i in seq_along(1:length(unique(curve_data[[group_id]])))){
             single_curve <- 
-            curve_data[curve_data[[group_id]] == 
-                       unique(curve_data[[group_id]])[i],]
+                  curve_data[curve_data[[group_id]] == 
+                                   unique(curve_data[[group_id]])[i],]
             plot(
                   single_curve[[Photo]] ~ single_curve[[PARi]] ,
                   xlim = c(-2, max(curve_data[[PARi]])), 
@@ -50,5 +94,12 @@ diagnostic_AQ_plot <- function(curve_data, fit_data, Photo, PARi, group_id){
             col = "red",
             lwd = 2
             )
-      }
+            }
+      dev.off()
+             } else {
+            return(
+                  "Warning: the file path provided to save_path does not exist"
+            )}
+            
+}
 }
