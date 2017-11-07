f6f18514d4b3918121eac92b38248495bf12d9de
Nick Tomeo <tomeonj@gmail.com>
2017-11-07 15:37:51 -0500

    Default model fitting now using onls::onls()
    The function originally used base::nls(), and only nls(), to fit the model. Now the default is to use orthogonal difference regression with the onls::onls() function. The base::nls() model can still be used by explicitly asking for it with the argument fit_type = "nls".

--- AQ_curve_function.R
+++ AQ_curve_function.R
@@ -40,7 +40,7 @@
 #                             PARi = "PARi", 
 #                             group_id = "group_id")
 
-fit_AQ_curve <- function(df, group_id, Photo, PARi){
+fit_AQ_curve <- function(df, group_id, Photo, PARi, fit_type = "onls"){
       AQ_curve_fits <- data.frame(ID = character(),
                                   Asat = numeric(),
                                   Phi = numeric(),
@@ -52,78 +52,166 @@ fit_AQ_curve <- function(df, group_id, Photo, PARi){
                                   Q_sat_85 = numeric(),  
                                   stringsAsFactors = FALSE
       )
+      if(fit_type == "onls"){
+      if(require("onls")){
+            print("onls is loaded correctly")
+      } else {
+            print("trying to install onls")
+            install.packages("onls")
+            if(require(lme4)){
+                  print("onls installed and loaded")
+            } else {
+                  stop("could not install onls")
+            }
+      }
+      library("onls")      
       for(i in seq_along(unique(df[[group_id]]))){
             tryCatch({
-            AQ_curve_fits[i, 1] <- unique(df[[group_id]])[i]
-            # Subset by group_ID iteratively:
-            single_curve1 <- df[df[[group_id]] == unique(df[[group_id]])[i],]
-            single_curve1$assim <- single_curve1[[Photo]]
-            single_curve1$PAR <- single_curve1[[PARi]]
-            # Reorder to assure PAR goes from low to high, allowing for the 
-                  # preliminary phi.as.slope estimate two lines below.
-            single_curve <- single_curve1[order(single_curve1$PAR),]
-            phi.as.slope <- with(single_curve, # use as starting value
-                                    as.numeric(coef(lm(
-                                          assim[1:5] ~ PAR[1:5]))[2]))
-            # Fit the curve:
-            temp.fit <- with(single_curve, # use the subset of a single curve
-                             nls(assim ~ ((Phi * PAR + Asat - 
-                                                 sqrt((Phi * PAR + Asat)^2 - 
-                                                            4 * Phi * theta * 
-                                                            Asat * PAR ))
-                             )/(2*theta) - Rd,
-                             start=list(
-                                   Asat = (max(assim)),
-                                   Phi = phi.as.slope,
-                                   Rd = -min(assim),
-                                   theta = 0.5),
-                             control = list(maxiter = 50),
-                             algorithm = "port")
-            )
-            AQ_curve_fits[i, 2] <- as.numeric(coef(temp.fit)[1]) # asat 
-            AQ_curve_fits[i, 3] <- as.numeric(coef(temp.fit)[2]) # Phi
-            AQ_curve_fits[i, 4] <- as.numeric(coef(temp.fit)[3]) # Rd
-            AQ_curve_fits[i, 5] <- as.numeric(coef(temp.fit)[4]) # theta
-            AQ_curve_fits[i, 6] <- sum(resid(temp.fit)^2)
-            AQ_curve_fits[i, 7] <- (as.numeric(coef(temp.fit)[3]) *(
-                  as.numeric(coef(temp.fit)[3]) * as.numeric(coef(temp.fit)[4]) - 
-                                                                   as.numeric(coef(temp.fit)[1]))
-            ) / (as.numeric(coef(temp.fit)[2]) * (
-                  as.numeric(coef(temp.fit)[3]) - as.numeric(coef(temp.fit)[1])
-            ))
-            AQ_curve_fits[i, 8] <- (
-                  (as.numeric(coef(temp.fit)[1]) * 0.75 + 
-                         (as.numeric(coef(temp.fit)[3]))) * (
-                               as.numeric(coef(temp.fit)[1]) * 0.75 *
-                                     as.numeric(coef(temp.fit)[4]) +
-                                     as.numeric(coef(temp.fit)[3]) *
-                                     as.numeric(coef(temp.fit)[4]) -
-                                     as.numeric(coef(temp.fit)[1])
-                         )) / (
-                               as.numeric(coef(temp.fit)[2])* (
-                                     as.numeric(coef(temp.fit)[1]) * 0.75 +
-                                           as.numeric(coef(temp.fit)[3]) -
+                  AQ_curve_fits[i, 1] <- unique(df[[group_id]])[i]
+                  # Subset by group_ID iteratively:
+                  single_curve1 <- df[df[[group_id]] == unique(df[[group_id]])[i],]
+                  single_curve1$assim <- single_curve1[[Photo]]
+                  single_curve1$PAR <- single_curve1[[PARi]]
+                  single_curve = single_curve1[order(single_curve1$PAR),]
+                  phi.as.slope <- with(single_curve,
+                                       as.numeric(coef(lm(
+                                             assim[1:5] ~ PAR[1:5]))[2]))
+                  # Fit the curve:
+                  temp.fit <- with(single_curve, # use the subset of a single curve
+                                   onls(assim ~ ((Phi * PAR + Asat - 
+                                                       sqrt((Phi * PAR + Asat)^2 - 
+                                                                  4 * Phi * theta * 
+                                                                  Asat * PAR ))
+                                   )/(2*theta) - Rd,
+                                   start=list(
+                                         Asat = (max(assim)),
+                                         Phi = phi.as.slope,
+                                         Rd = -min(assim),
+                                         theta = 0.5),
+                                   control = list(maxiter = 50)#,
+                                   #algorithm = "port"
+                                   )
+                  )
+                  AQ_curve_fits[i, 2] <- as.numeric(coef(temp.fit)[1]) # asat 
+                  AQ_curve_fits[i, 3] <- as.numeric(coef(temp.fit)[2]) # Phi
+                  AQ_curve_fits[i, 4] <- as.numeric(coef(temp.fit)[3]) # Rd
+                  AQ_curve_fits[i, 5] <- as.numeric(coef(temp.fit)[4]) # theta
+                  AQ_curve_fits[i, 6] <- sum(resid(temp.fit)^2)
+                  AQ_curve_fits[i, 7] <- (as.numeric(coef(temp.fit)[3]) *(
+                        as.numeric(coef(temp.fit)[3]) * as.numeric(coef(temp.fit)[4]) - 
+                              as.numeric(coef(temp.fit)[1]))
+                  ) / (as.numeric(coef(temp.fit)[2]) * (
+                        as.numeric(coef(temp.fit)[3]) - as.numeric(coef(temp.fit)[1])
+                  ))
+                  AQ_curve_fits[i, 8] <- (
+                        (as.numeric(coef(temp.fit)[1]) * 0.75 + 
+                               (as.numeric(coef(temp.fit)[3]))) * (
+                                     as.numeric(coef(temp.fit)[1]) * 0.75 *
+                                           as.numeric(coef(temp.fit)[4]) +
+                                           as.numeric(coef(temp.fit)[3]) *
+                                           as.numeric(coef(temp.fit)[4]) -
                                            as.numeric(coef(temp.fit)[1])
-                               ))
-            
-            AQ_curve_fits[i, 9] <- (
-                  (as.numeric(coef(temp.fit)[1]) * 0.85 + 
-                         (as.numeric(coef(temp.fit)[3]))) * (
-                               as.numeric(coef(temp.fit)[1]) * 0.85 *
-                                     as.numeric(coef(temp.fit)[4]) +
-                                     as.numeric(coef(temp.fit)[3]) *
-                                     as.numeric(coef(temp.fit)[4]) -
-                                     as.numeric(coef(temp.fit)[1])
-                         )) / (
-                               as.numeric(coef(temp.fit)[2])* (
-                                     as.numeric(coef(temp.fit)[1]) * 0.85 +
-                                           as.numeric(coef(temp.fit)[3]) -
+                               )) / (
+                                     as.numeric(coef(temp.fit)[2])* (
+                                           as.numeric(coef(temp.fit)[1]) * 0.75 +
+                                                 as.numeric(coef(temp.fit)[3]) -
+                                                 as.numeric(coef(temp.fit)[1])
+                                     ))
+                  
+                  AQ_curve_fits[i, 9] <- (
+                        (as.numeric(coef(temp.fit)[1]) * 0.85 + 
+                               (as.numeric(coef(temp.fit)[3]))) * (
+                                     as.numeric(coef(temp.fit)[1]) * 0.85 *
+                                           as.numeric(coef(temp.fit)[4]) +
+                                           as.numeric(coef(temp.fit)[3]) *
+                                           as.numeric(coef(temp.fit)[4]) -
                                            as.numeric(coef(temp.fit)[1])
-                               ))
-      }, error = function(E){cat("Error: ", conditionMessage(E), "\n")})
+                               )) / (
+                                     as.numeric(coef(temp.fit)[2])* (
+                                           as.numeric(coef(temp.fit)[1]) * 0.85 +
+                                                 as.numeric(coef(temp.fit)[3]) -
+                                                 as.numeric(coef(temp.fit)[1])
+                                     ))
+            }, error = function(E){cat("Error: ", conditionMessage(E), "\n")})
       }
       return(AQ_curve_fits)
+      } else{
+      if(fit_type == "nls"){
+            for(i in seq_along(unique(df[[group_id]]))){
+                  tryCatch({
+                  AQ_curve_fits[i, 1] <- unique(df[[group_id]])[i]
+                  # Subset by group_ID iteratively:
+                  single_curve1 <- df[df[[group_id]] == unique(df[[group_id]])[i],]
+                  single_curve1$assim <- single_curve1[[Photo]]
+                  single_curve1$PAR <- single_curve1[[PARi]]
+                  single_curve = single_curve1[order(single_curve1$PAR),]
+                  phi.as.slope <- with(single_curve,
+                                       as.numeric(coef(lm(
+                                             assim[1:5] ~ PAR[1:5]))[2]))
+                  # Fit the curve:
+                  temp.fit <- with(single_curve, 
+                                   nls(assim ~ ((Phi * PAR + Asat - 
+                                           sqrt((Phi * PAR + Asat)^2 - 
+                                                4 * Phi * theta * 
+                                                      Asat * PAR ))
+                                               )/(2*theta) - Rd,
+                                               start=list(
+                                                     Asat = (max(assim)),
+                                                     Phi = phi.as.slope,
+                                                     Rd = -min(assim),
+                                                     theta = 0.5),
+                                               control = list(maxiter = 50),
+                                               algorithm = "port")
+                              )
+                  AQ_curve_fits[i, 2] <- as.numeric(coef(temp.fit)[1]) # asat 
+                  AQ_curve_fits[i, 3] <- as.numeric(coef(temp.fit)[2]) # Phi
+                  AQ_curve_fits[i, 4] <- as.numeric(coef(temp.fit)[3]) # Rd
+                  AQ_curve_fits[i, 5] <- as.numeric(coef(temp.fit)[4]) # theta
+                  AQ_curve_fits[i, 6] <- sum(resid(temp.fit)^2)
+                  AQ_curve_fits[i, 7] <- (as.numeric(coef(temp.fit)[3]) *(
+                              as.numeric(coef(temp.fit)[3]) * 
+                              as.numeric(coef(temp.fit)[4]) - 
+                              as.numeric(coef(temp.fit)[1]))
+                              ) / (as.numeric(coef(temp.fit)[2]) * (
+                              as.numeric(coef(temp.fit)[3]) - 
+                                    as.numeric(coef(temp.fit)[1])
+                              ))
+                  AQ_curve_fits[i, 8] <- (
+                              (as.numeric(coef(temp.fit)[1]) * 0.75 + 
+                              (as.numeric(coef(temp.fit)[3]))) * (
+                              as.numeric(coef(temp.fit)[1]) * 0.75 *
+                              as.numeric(coef(temp.fit)[4]) +
+                              as.numeric(coef(temp.fit)[3]) *
+                              as.numeric(coef(temp.fit)[4]) -
+                              as.numeric(coef(temp.fit)[1])
+                              )) / (
+                              as.numeric(coef(temp.fit)[2])* (
+                              as.numeric(coef(temp.fit)[1]) * 0.75 +
+                              as.numeric(coef(temp.fit)[3]) -
+                              as.numeric(coef(temp.fit)[1])
+                              ))
+                  AQ_curve_fits[i, 9] <- (
+                              (as.numeric(coef(temp.fit)[1]) * 0.85 + 
+                              (as.numeric(coef(temp.fit)[3]))) * (
+                              as.numeric(coef(temp.fit)[1]) * 0.85 *
+                              as.numeric(coef(temp.fit)[4]) +
+                              as.numeric(coef(temp.fit)[3]) *
+                              as.numeric(coef(temp.fit)[4]) -
+                              as.numeric(coef(temp.fit)[1])
+                              )) / (
+                              as.numeric(coef(temp.fit)[2])* (
+                              as.numeric(coef(temp.fit)[1]) * 0.85 +
+                              as.numeric(coef(temp.fit)[3]) -
+                              as.numeric(coef(temp.fit)[1])
+                              ))
+                        }, error = function(E){
+                              cat("Error: ", conditionMessage(E), "\n")})
+                  }
+                  return(AQ_curve_fits)      
+            } else{print("ERROR: 'fit_type' specified incorrectly.")}
+      }
 }
+
 
 
 ### Details ###
